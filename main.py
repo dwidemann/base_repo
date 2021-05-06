39e998abbc15af30caf147f5bed14aa733c1277a
Widemann, David Peter <widemann1@llnl.gov>
2021-05-06 13:02:31 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+
+import argparse
+from parse_config import ConfigParser
+import collections
+import pprint
+
+def main(config):
+    logger = config.get_logger('trainer', config['trainer']['verbosity'])
+    # print logged informations to the screen
+    pprint.pprint(config.config)
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PyTorch Template')
+    args.add_argument('-c', '--config', default=None, type=str,
+                      help='config file path (default: None)')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to latest checkpoint (default: None)')
+    args.add_argument('-d', '--device', default=None, type=str,
+                      help='indices of GPUs to enable (default: all)')
+
+    # custom cli options to modify configuration from default values given in json file.
+    CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
+    options = [
+        CustomArgs(['--lr', '--learning_rate'], type=float, target='optimizer;args;lr'),
+        CustomArgs(['--bs', '--batch_size'], type=int, target='data_loader;args;batch_size')
+    ]
+    config = ConfigParser.from_args(args, options)
+    main(config)
